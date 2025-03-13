aa05ded967d4e50e9551298e2fd512420e2e98a1
greenduzt <chamara_sachintha@yahoo.com>
2025-03-13 13:22:34 +1100

    MassTransit added and Contracts class lib created

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,5 +1,5 @@
 using AuctionService.Data;
-using AuctionService.Entities;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +12,11 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>{
 });
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMassTransit(x => {
+    x.UsingRabbitMq((context, cfg) => {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
