5f0e71cff7400d64e783b3c199c56aeafc1a9438
greenduzt <chamara_sachintha@yahoo.com>
2025-05-12 16:02:41 +1000

    IdentityServer user data seeded

--- src/IdentityService/Program.cs
+++ src/IdentityService/Program.cs
@@ -25,23 +25,8 @@ try
 
     // this seeding is only for the template to bootstrap the DB and users.
     // in production you will likely want a different approach.
-    if (args.Contains("/seed"))
-    {
-        Log.Information("Seeding database...");
-        SeedData.EnsureSeedData(app);
-        Log.Information("Done seeding database. Exiting.");
-        return;
-    }
-
-    if (app.Environment.IsDevelopment())
-    {
-        app.Lifetime.ApplicationStopping.Register(() =>
-        {
-            var usage = app.Services.GetRequiredService<LicenseUsageSummary>();
-            Console.Write(Summary(usage));
-            Console.ReadKey();
-        });
-    }
+   
+    SeedData.EnsureSeedData(app);
 
     app.Run();
 }
